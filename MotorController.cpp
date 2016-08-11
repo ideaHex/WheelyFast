6a0c1d6742b542e853a76bc3811781662563c80a
ideaHex <damian@ideahex.com>
2016-08-11 21:41:57 +0930

    Initial commit

---
+++ MotorController.cpp
@@ -0,0 +1,173 @@
+/*
+ * 
+ * Remember on the ESP8266 pin 16, also called D0 has no PWM output so don't use it for speed/pwm
+ * This is a work in progress
+ */
+
+#include "MotorController.h"
+
+
+// constructor for h bridge with enable pins for PWM
+motorController::motorController(uint8_t motorA_pin , uint8_t PWM_A_pin,uint8_t motorB_pin , uint8_t PWM_B_pin)
+{
+  analogWriteFreq(PWMFrequency); //Theoretical max frequency is 80000000/range, range = 1024 so 78Khz here
+  mPA1 = motorA_pin;
+  //mPA2 = motorA_pin_2;
+  mPB1 = motorB_pin;
+  //mPB2 = motorB_pin_2;
+  PWMA = PWM_A_pin;
+  PWMB = PWM_B_pin;
+  useEnablePins = true;
+  pinMode(mPA1,OUTPUT);
+  //pinMode(mPA2,OUTPUT);
+  pinMode(mPB1,OUTPUT);
+  //pinMode(mPB2,OUTPUT);
+  pinMode(PWMA,OUTPUT);
+  pinMode(PWMB,OUTPUT);
+}
+// constructor for h bridge without enable pins for PWM
+// can also be used for h bridge with direction and speed(PWM) pins using (directionA,Unused pin,SpeedA,directionB,Unused pin,SpeedB);
+//motorController::motorController(uint8_t motorA_pin , uint8_t motorA_pin_2 ,uint8_t motorB_pin , uint8_t motorB_pin_2){
+//  analogWriteFreq(PWMFrequency); //Theoretical max frequency is 80000000/range, range = 1024 so 78Khz here
+//  mPA1 = motorA_pin;
+//  mPA2 = motorA_pin_2;
+//  mPB1 = motorB_pin;
+//  mPB2 = motorB_pin_2;
+//  useEnablePins = false;
+//  pinMode(mPA1,OUTPUT);
+//  pinMode(mPA2,OUTPUT);
+//  pinMode(mPB1,OUTPUT);
+//  pinMode(mPB2,OUTPUT);
+//}
+void motorController::update(int X, int Y){
+  if (X > MAX_range)X = MAX_range;
+  if (X < -MAX_range)X = -MAX_range;
+  if (Y > MAX_range)Y = MAX_range;
+  if (Y < -MAX_range)Y = -MAX_range;
+  int A = Y - int(steeringSensitivity*(X*(Y<=0) - X*(Y>0))); // to fix steering when backwards *(Y<0) + X*(Y>0)
+  int B = Y + int(steeringSensitivity*(X*(Y<=0) - X*(Y>0)));
+  if (!useEnablePins){// assuming LOW LOW to h bridge is OFF and HIGH HIGH is Break
+    
+    if (!reverseMotorADirection)
+    {
+      analogWrite(mPA1, getPWM1(A,trimA));
+      //analogWrite(mPA2, getPWM2(A,trimA));
+    }
+    else
+    {
+      analogWrite(mPA1, getPWM2(A,trimA));
+      //analogWrite(mPA2, getPWM1(A,trimA));
+    }
+    //Serial.printf("Motor A: 1 %d , 2 %d \r\n",int(getPWM1(A)*trimA),int(getPWM2(A)*trimA));
+    //Serial.printf("Motor B: 1 %d , 2 %d \r\n",int(getPWM1(B)*trimB),int(getPWM2(B)*trimB));
+    
+    if (!reverseMotorBDirection)
+    {
+      analogWrite(mPB1, getPWM1(B,trimB));
+      //analogWrite(mPB2, getPWM2(B,trimB));
+    }
+    else
+    {
+      analogWrite(mPB1, getPWM2(B,trimB));
+      //analogWrite(mPB2, getPWM1(B,trimB));
+    }
+    
+  }else{ // PWM pins connected to H-Bridge enable
+    
+    if (!reverseMotorADirection)
+    {
+      digitalWrite(mPA1, (A<0));
+      //digitalWrite(mPA2, (A>0));
+    }
+    else
+    {
+      digitalWrite(mPA1, (A>0));
+      //digitalWrite(mPA2, (A<0));
+    }
+    if (A<0) A *= -1;
+    analogWrite(PWMA, getPWM1(A,trimA));
+    //Serial.printf("Motor A PWM: %d  \r\n", getPWM1(A,trimA)); 
+    if (!reverseMotorBDirection)
+    {
+      digitalWrite(mPB1, (B>0));
+      //digitalWrite(mPB2, (B<0));
+    }
+    else
+    {
+      digitalWrite(mPB1, (B<0));
+      //digitalWrite(mPB2, (B>0));
+    }
+    if (B<0) B *= -1;
+    analogWrite(PWMB, getPWM1(B,trimB));
+    //Serial.printf("Motor B PWM: %d \r\n", getPWM1(B,trimB));
+  }
+}
+// maximum input expected from controller input ie 0 to Range
+void motorController::setRange(int newRange)
+{
+  MAX_range = newRange;
+}
+
+int motorController::getPWM1(int A, float trimN)
+{
+  int result = (A>0) * ( ((float(float(A)/float(MAX_range))*PWMWriteRange)* (A<MAX_range)) + ((A>=MAX_range) * PWMWriteRange) );
+  result = int(checkMinimumSpeed(result, trimN) * trimN);
+  return result;
+}
+
+int motorController::getPWM2(int A, float trimN)
+{
+  int result = (A<0) * ( ((float(float(A*-1)/float(MAX_range))*PWMWriteRange)* ((A*-1)<MAX_range)) + (((A*-1)>=MAX_range) * PWMWriteRange) );
+  result = int(checkMinimumSpeed(result, trimN) * trimN);
+  return result;
+}
+
+void motorController::reverseMotorA()
+{
+  reverseMotorADirection = !reverseMotorADirection;
+}
+
+void motorController::reverseMotorB(){
+    reverseMotorBDirection = !reverseMotorBDirection;
+}
+
+// trim output for motor A & B to compensate for slightly different motors or reduce max - speed
+// 0 - 1, 0 = stop 1 = 100 %
+void motorController::setTrim(float trimForMotorA ,float trimForMotorB){
+  trimA = checkNormal(trimForMotorA);
+  trimB = checkNormal(trimForMotorB);
+  if (trimA<minMotorSpeed)trimA=minMotorSpeed;
+  if (trimB<minMotorSpeed)trimB=minMotorSpeed;
+}
+// reduce turning speed
+void motorController::setSteeringSensitivity(float sensitivity){
+  steeringSensitivity = checkNormal(sensitivity);
+}
+
+float motorController::checkNormal(float normal){
+  if (normal > 1)normal = 1.0;
+  if (normal < 0)normal = 0.0;
+  return normal;
+}
+void motorController::setPWMFrequency(int frequency){
+  PWMFrequency = frequency;
+  analogWriteFreq(PWMFrequency);
+}
+void motorController::setPWMFrequency(int frequency, int range){
+  PWMWriteRange = range;
+  PWMFrequency = frequency;
+  analogWriteRange(PWMWriteRange);
+  analogWriteFreq(PWMFrequency);
+}
+int motorController::checkMinimumSpeed(int PWM, float trimN){
+  if (PWM > 0)PWM = int(PWM*(1.0-minMotorSpeed/trimN) + PWMWriteRange * minMotorSpeed /trimN);
+  if (PWM > PWMWriteRange)PWM = PWMWriteRange;
+  return PWM;
+}
+// motor speed expressed as a value of 0.0 - 1.0
+void motorController::setMinimumSpeed(float minimumMotorSpeed){
+  minMotorSpeed = checkNormal(minimumMotorSpeed);
+  if (trimA<minMotorSpeed)trimA=minMotorSpeed;
+  if (trimB<minMotorSpeed)trimB=minMotorSpeed;
+}
+
---
+++ WheelyFast.ino
@@ -0,0 +1,229 @@
+/*
+ * 
+ * 
+ */
+
+ 
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <Ticker.h>
+#include "MotorController.h"
+#include "US100Ping.h"
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// WebPage
+#include "WebPage.h"
+
+extern "C" { 
+   #include "user_interface.h" 
+ } 
+
+//////////////////////
+// RGB LEDs //
+//////////////////////
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+#define PIN D3
+#define NUMPIXELS      1
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int delayval = 500; // delay for half a second
+            
+
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char *password = "12345678";      // This is the Wifi Password (only numbers and letters,  not . , |)
+String AP_Name = "FH@Tbot";             // This is the Wifi Name(SSID), some numbers will be added for clarity (mac address)
+bool enableCompatibilityMode = false;   // turn on compatibility mode for older devices, spacifically sets no encryption and 11B wifi standard
+
+void setupWiFi(void);
+void initHardware(void);
+
+/////////////////////
+// Pin Definitions //
+/////////////////////
+
+// stepper with direction and speed pins, don't use D0 for speed
+const int motorLeftDir  = D7;
+const int motorLeftSpd  = D8;
+const int motorRightDir = D1;
+const int motorRightSpd = D2;
+
+motorController motors(motorLeftDir,motorLeftSpd,motorRightDir,motorRightSpd); 
+
+
+WiFiServer server(80);                  // http only https is 443
+WiFiClient client;
+DNSServer dnsServer;
+Ticker HeartBeatTicker;
+//US100Ping ping;
+byte clientTimeout = 150;
+bool clientStopped = true;
+unsigned long nextClientTimeout = 0;
+
+bool HeartBeatRcvd = false;
+
+void Stop(void)
+{
+  motors.update(0,0);
+}
+
+void CheckHeartBeat(void)
+{
+  if (HeartBeatRcvd == true)
+  {
+    HeartBeatRcvd = false;
+  }
+  else
+  {
+    Stop();
+    // Serial.println("Connection lost STOP!!!!!!");
+  }
+}
+
+void setup()
+{
+  //system_update_cpu_freq(160);        // set cpu to 160MHZ !
+  initHardware();
+  setupWiFi();
+  HeartBeatTicker.attach_ms(500, CheckHeartBeat);
+  Stop();
+  //motors.setTrim(1.0,1.0);            // this setting is optional, it compensates for speed difference of motors eg (0.95,1.0), and it can reduce maximum speed of both eg (0.75,0.75);
+  //motors.setSteeringSensitivity(0.25);  // this setting is optional
+  motors.setPWMFrequency(20000);           // this setting is optional, depending on power supply and H-Bridge this option will alter motor noise and torque.
+  //motors.setMinimumSpeed(0.10);         // this setting is optional, default is 0.1(10%) to prevent motor from stalling at low speed
+    
+  pixels.begin();
+  pixels.show(); // Initialize all pixels to 'off'
+//     //////////////////////////
+// //for(int i=0;i<NUMPIXELS;i++){
+//
+//    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(0, pixels.Color(50,0,80)); // Moderately bright green color.
+//    pixels.setPixelColor(1, pixels.Color(0,0,0));
+//    pixels.setPixelColor(2, pixels.Color(150,0,20));
+//    pixels.setPixelColor(3, pixels.Color(150,0,20));
+//    pixels.setPixelColor(4, pixels.Color(150,0,20));
+//    pixels.setPixelColor(5, pixels.Color(150,0,20));
+//
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+//  }
+  /////////////////////////////////////////////
+  
+  }
+
+void loop()
+{
+  // time dependant functions here
+//   ping.run();
+//   if (ping.gotTemperature()) Serial.printf("Temperature: %d C \r\n", ping.getTemperature());
+//   if (ping.gotDistance()) Serial.printf("distance: %d mm \r\n", ping.getDistance());
+   dnsServer.processNextRequest();
+
+   // client functions here
+   if (clientStopped){
+    client = server.available();
+    clientStopped = false;
+    nextClientTimeout = millis() + clientTimeout;
+   }else{
+    if (!client.connected() || millis() > nextClientTimeout){
+      client.stop();
+      clientStopped = true;
+    }
+   }
+   
+  if (!client.available())
+  {
+    return;
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  //Serial.println(req);
+  //Serial.println(client.readString());
+  client.flush();
+  int indexOfX = req.indexOf("/X");
+  int indexOfY = req.indexOf("/Y");
+  if (indexOfX != -1 && indexOfY != -1){
+    
+    String xOffset = req.substring(indexOfX + 2, indexOfX + 8);
+    int dX = xOffset.toInt();
+    String yOffset = req.substring(indexOfY + 2, indexOfY + 8);
+    int dY = yOffset.toInt();
+    
+    motors.update(dX,dY);
+    
+    //Serial.print(F("DX: "));
+    //Serial.print(dX);
+    //Serial.print(F(" DY: "));
+    //Serial.println(dY);
+    //Serial.print(F("Free Ram: "));
+    //Serial.println(system_get_free_heap_size());
+
+    HeartBeatRcvd = true;               // recieved data, must be connected
+
+  }else{
+        if (req.indexOf("GET / HTTP/1.1") != -1){
+            Serial.println(F("Sending Page"));
+            client.write_P(HTML_text,strlen_P(HTML_text));
+            delay(1);                   // to improve compatability with some browsers
+          }
+        }
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+ 
+  // Create a unique name by appending the MAC address to the AP Name
+
+  AP_Name = AP_Name + " " + WiFi.softAPmacAddress();
+  char AP_NameChar[AP_Name.length() + 1];
+  AP_Name.toCharArray(AP_NameChar,AP_Name.length() + 1);
+
+  // setup AP, start DNS server, start Web server
+
+  int channel = random(1,13);
+  const byte DNS_PORT = 53;
+  IPAddress subnet(255, 255, 255, 0);
+  IPAddress apIP(192, 168, 1, 1);
+  WiFi.softAPConfig(apIP, apIP, subnet);
+  if (enableCompatibilityMode){
+    wifi_set_phy_mode(PHY_MODE_11B);    // Note: ESP8266 soft-AP only support bg.
+    const char *pw = "";
+    WiFi.softAP(AP_NameChar, pw , channel , 0 );
+  }else{
+    wifi_set_phy_mode(PHY_MODE_11N);
+    WiFi.softAP(AP_NameChar, password , channel , 0 );
+  }
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "FHTbot.com", apIP);//must use '.com, .org etc..' and cant use '@ or _ etc...' ! . Use "*" to divert all **VALID** names
+  server.begin();
+  
+  //WiFi.printDiag(Serial);
+}
+
+void initHardware()
+{
+  Serial.begin(115200);
+  delay(100);
+  Serial.println(F("\r\n"));
+  Serial.println(F("            FH@Tbot Serial Connected\r\n"));
+  Serial.println(F("  Type \"FHTbot.com\" into your browser to connect. \r\n"));
+  //Serial.swap();
+  //ping.begin(D7,D8,9600);
+  //ping.begin(Serial);
+}
